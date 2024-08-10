8ffc79c0db1dd6434e871b5ef21e906a0c3d5f91
hank-butler <hank.butler@protonmail.com>
2024-08-09 21:25:34 -0600

    [SPRINT 2] all tasks, catching up

---
+++ auth-service/src/domain/mod.rs
@@ -0,0 +1,11 @@
+pub mod data_stores;
+pub mod email;
+pub mod password;
+pub mod error;
+pub mod user;
+
+pub use data_stores::*;
+pub use email::*;
+pub use password::*;
+pub use error::*;
+pub use user::*;
--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,38 +1,69 @@
 use std::error::Error;
-use axum::{routing::post, serve::Serve, Router};
+
+use app_state::AppState;
+
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    serve::Serve,
+    Json, Router
+};
+use domain::AuthAPIError;
 use routes::{login, logout, signup, verify_2fa, verify_token};
-use tower_http::services::ServeDir;
+use serde::{Deserialize, Serialize};
+use twoer_http::services::ServeDir;
 
-
+pub mod app_state;
+pub mod domain;
 pub mod routes;
-
+put mod services;
 
 pub struct Application {
     server: Serve<Router, Router>,
-    pub address: String,
+    pub address: String
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
             .route("/signup", post(signup))
             .route("/login", post(login))
             .route("/verify-2fa", post(verify_2fa))
-            .route("/logout", post(logout))
-            .route("/verify-token", post(verify_token));
-
+            .route("/verify-token", post(verify_token))
+            .with_state(app_state);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
-        // Create a new application instance and return it
         Ok(Application { server, address })
     }
 
     pub async fn run(self) -> Result<(), std::io::Error> {
-        println!("listening on {}", &self.address);
+        println!("listening on {}", $self.address);
         self.server.await
+    }
+}
+
+#[derive(Seriealize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status: StatusCode, error_message: &str) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid Credentials"),
+            AuthAPIError::UnexpectedError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected Error")
+            }
+        };
+        let body = Json(ErrorResponse{
+            error: error_message.to_string(),
+        });
+        (status, body).into_response()
     }
 }
